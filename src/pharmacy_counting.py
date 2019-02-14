a555452f0fb8c263b97f7ec43c2446134efad1d4
nanonothing <36753861+nanonothing@users.noreply.github.com>
2019-02-14 13:08:55 -0800

    Update pharmacy_counting.py

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -1,5 +1,10 @@
-
 import pandas as pd
-
-data = pd.read_csv('output_list.txt', header = None)
-print data
+import numpy as np
+import urllib
+import csv
+url="https://raw.githubusercontent.com/InsightDataScience/pharmacy_counting/master/insight_testsuite/tests/test_1/input/itcont.txt"
+loc="C:\\Users\\G Liu\\Documents\\Python Scripts\\itcont.cvs"
+urllib.request.urlretrieve(url, loc)
+Pharm=pd.read_csv(loc)
+Pharm.columns = ["id", "Last Name", "First Name", "drug Name", "drug cost"]
+print(Pharm)
