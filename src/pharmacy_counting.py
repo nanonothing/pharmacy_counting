16d274efdcd0052e93b50847be1c70462fd3fc70
nanonothing <36753861+nanonothing@users.noreply.github.com>
2019-02-15 17:06:21 -0800

    Update pharmacy_counting.py

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -9,33 +9,44 @@ Pharm=pd.read_csv(loc)
 Pharm.columns = ["id", "Last_Name", "First_Name", "drug_name", "drug_cost"]
 print(Pharm)
 #find the number of rows in the list
-row_count = sum(1 for row in Pharm)
-drug_count=0
+#row_count = sum(1 for row in Pharm)
+
 #define class to store drug name, number of prescriber and total cost
 class drug:
-    def __init__(self, name):
+    def __init__(self, name ,cost):
         self.name=name
-        self.num_prescriber=0
-        self.total_cost=0
+        self.num_prescriber=1
+        self.total_cost=cost
 
-#check
+#drug_list: use this dictionary to store all distinct drugs
+#drug_count: index of distinct drugs
+drug_count=0        
+drug_list={drug_count:"drug name"}        
 
-drug_list={drug_count:"drug name"}
+#output_data: list of all drug class element
+output_data=[]
+output_data.append(drug("drug name",0))
 
+#read the input data row by row
 for index, row in Pharm.iterrows():
     x=row["drug_name"]
+#if the drug name already appeared in previous row, add 1 to number of prescriber
+# and add drug cost to total cost
     if x in drug_list.values():
-       drug(x).num_prescriber=drug(x).num_prescriber+1
-       drug(x).total_cost=drug(x).total_cost+row["drug_cost"]
+       output_data[drug_count].num_prescriber=output_data[drug_count].num_prescriber+1
+       output_data[drug_count].total_cost=output_data[drug_count].total_cost+row["drug_cost"]
+#if the drug name is new, creat new class object drug(x,cost) and update drug_count,drug_list
     else:
-         drug_count=drug_count+1
-         drug(x)
-         drug_list.update({drug_count:x})
+        drug_count=drug_count+1
+        output_data.append(drug(x, row["drug_cost"]))
+        drug_list.update({drug_count:x})
        
  
- #create x.drug("name")
+
 print(drug_list)
 for i in range(drug_count+1):
-   x=drug_list[i]
-   print(x,drug(x).num_prescriber,drug(x).total_cost) 
+   print(output_data[i].name,output_data[i].num_prescriber,output_data[i].total_cost) 
+   
+
+        
    
